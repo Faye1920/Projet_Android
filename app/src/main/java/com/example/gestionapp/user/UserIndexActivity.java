03e8733257d8606bff8ce51abdc84927748532db
faye1920 <faywong1920@gmail.com>
2023-10-18 18:04:50 -0400

    add navigation drawer

--- app/src/main/java/com/example/gestionapp/user/UserIndexActivity.java
+++ app/src/main/java/com/example/gestionapp/user/UserIndexActivity.java
@@ -1,14 +1,63 @@
 package com.example.gestionapp.user;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.gestionapp.R;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class UserIndexActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.gestionapp.R;
+import com.example.gestionapp.user.type.ApplicationActivity;
+import com.example.gestionapp.user.type.CompteActivity;
+import com.example.gestionapp.user.type.GererActivity;
+import com.google.android.material.navigation.NavigationView;
+
+public class UserIndexActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_index);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        return true;
     }
 }
